975a9359b83c8daa8f22b02316d630d016533acf
EnglishEasy2021* <kevindermo@hotmail.com>
2021-08-09 18:44:49 -0600

    Se versionan avances en esquemas de usuario

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        min:6,
+        max:255
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+const schema = mongoose.model('User', userSchema);
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required(),
+
+});
+
+module.exports = {
+    schema,
+    schemaRegister
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,37 @@
 const router = require('express').Router();
+const User = require('../models/User').schema;
+const schemaRegister = require('../models/User').schemaRegister;
 
-router.post('/register', async (req, res) => {
 
-    res.json({
-        error: null,
-        data: 'aquí va ir la data'
-    })
-})
+router.post('/register', async(req, res) => {
+
+    const { error } = schemaRegister.validate(req.body);
+
+    if(error){
+        return res.status(400).json({
+            error: error.details[0].message
+        });
+    }
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password:req.body.password
+    });
+
+    try{
+        const userDB = await user.save();
+        res.json({
+            error: null,
+            data: userDB
+        })
+
+    } catch(error){
+        res.status(400).json(error);
+    }
+});
+
+
+
 
 module.exports = router;
